996e2509ee6ddc0a3474ac303711c4ae75908639
mariesaoconnell <mariesa.oconnell@gmail.com>
2023-05-28 21:53:36 -0400

    Fix responsiveness of header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import '../styles/header.css'
 import logo from '../images/DMZ.png'
-import { Ratio } from 'react-bootstrap';
 
 function Header(props) {
   return (
-    <div className='align-self-center' style={{width: 600, height: 'auto'}}>
-      <Ratio aspectRatio='16x9'>
-        <img src={logo} alt='logo'/>
-      </Ratio>
+    <div className='align-self-center'>
+        <img className='img-fluid' style={{maxHeight: 400}} src={logo} alt='logo'/>
     </div>
   );
 }
